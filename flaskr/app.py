37972931f270deddd6814afd6c9bb76ddb243b25
tomislav.vidovic <tomislav.vidovic@fer.hr>
2022-11-14 14:07:19 +0100

    Add runtime

--- flaskr/app.py
+++ flaskr/app.py
@@ -18,74 +18,72 @@ class SetEncoder(json.JSONEncoder):
             return list(obj)
         return json.JSONEncoder.default(self, obj)
 
-def create_app(test_config=None):
+# Create and configure app
+script_path = os.path.dirname(os.path.abspath(__file__))
+templates_path = os.path.join(script_path, "templates")
+app = Flask(__name__)
+# app = Flask(__name__, template_folder=templates_path, instance_relative_config=True)
+# app.config.from_mapping(
+#     SECRET_KEY="dev", DATABASE=os.path.join(app.instance_path, "flaskr.sqlite")
+# )
 
-    # Create and configure app
-    script_path = os.path.dirname(os.path.abspath(__file__))
-    templates_path = os.path.join(script_path, "templates")
-    app = Flask(__name__)
-    # app = Flask(__name__, template_folder=templates_path, instance_relative_config=True)
-    # app.config.from_mapping(
-    #     SECRET_KEY="dev", DATABASE=os.path.join(app.instance_path, "flaskr.sqlite")
-    # )
+# # Load instance from config file if exists
+# if test_config is None:
+#     app.config.from_pyfile("config.py", silent=True)
+# # Load test config if passed
+# else:
+#     app.config.from_mapping(test_config)
 
-    # # Load instance from config file if exists
-    # if test_config is None:
-    #     app.config.from_pyfile("config.py", silent=True)
-    # # Load test config if passed
-    # else:
-    #     app.config.from_mapping(test_config)
+# # Ensure the instance folder exists
+# try:
+#     os.makedirs(app.instance_path)
+# except OSError:
+#     pass
 
-    # # Ensure the instance folder exists
-    # try:
-    #     os.makedirs(app.instance_path)
-    # except OSError:
-    #     pass
+app.config['UPLOAD_FOLDER'] = "test_upload"
+ALLOWED_EXTENSIONS = 'xml'
 
-    app.config['UPLOAD_FOLDER'] = "test_upload"
-    ALLOWED_EXTENSIONS = 'xml'
+def save_file(file):
+    if file.filename.rsplit(".")[1] in ALLOWED_EXTENSIONS:
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    else:
+        raise Exception("Pogrešna datoteka je prenesena, mora biti tipa xml")
+# @app.route('/upload')
+# def upload_file():
+#     return render_template('upload.html')
 
-    def save_file(file):
-        if file.filename.rsplit(".")[1] in ALLOWED_EXTENSIONS:
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        else:
-            raise Exception("Pogrešna datoteka je prenesena, mora biti tipa xml")
-    # @app.route('/upload')
-    # def upload_file():
-    #     return render_template('upload.html')
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return 'Nema prenesene datoteke!'
+        file = request.files['file']
+        if file.filename == '':
+            return 'Nije odabrana datoteka'
+        try:
+            save_file(file)
+            return 'Unesen je riječnik!'
+        except Exception as e:
+            return 'Dogodila se greška '+ str(e)
+
+@app.route("/return")
+def return_entries():
+    list_dictionary = xml_reader.return_entries()
+    list_dictionary = json.dumps(list_dictionary, cls=SetEncoder, sort_keys=True, ensure_ascii=False)
+    return list_dictionary
+
+@app.route("/dict", methods=['GET'])
+def return_dict():
+    dictionary_attributes= set(xml_reader.main())
+    json_dictionary_attributes = json.dumps(dictionary_attributes, cls=SetEncoder, sort_keys=True, ensure_ascii=False)
+    return json_dictionary_attributes
+
+@app.route("/")
+def starting_page():
     
-    @app.route('/uploader', methods = ['GET', 'POST'])
-    def upload_file():
-        if request.method == 'POST':
-            if 'file' not in request.files:
-                return 'Nema prenesene datoteke!'
-            file = request.files['file']
-            if file.filename == '':
-                return 'Nije odabrana datoteka'
-            try:
-                save_file(file)
-                return 'Unesen je riječnik!'
-            except Exception as e:
-                return 'Dogodila se greška '+ str(e)
+    return render_template('home.html')
+# return app
 
-    @app.route("/return")
-    def return_entries():
-        list_dictionary = xml_reader.return_entries()
-        list_dictionary = json.dumps(list_dictionary, cls=SetEncoder, sort_keys=True, ensure_ascii=False)
-        return list_dictionary
-
-    @app.route("/dict", methods=['GET'])
-    def return_dict():
-        dictionary_attributes= set(xml_reader.main())
-        json_dictionary_attributes = json.dumps(dictionary_attributes, cls=SetEncoder, sort_keys=True, ensure_ascii=False)
-        return json_dictionary_attributes
-
-    @app.route("/")
-    def starting_page():
-        
-        return render_template('home.html')
-    return app
-
-if __name__ == '__main__':
-    create_app().run()
+# if __name__ == '__main__':
+#     create_app().run()
